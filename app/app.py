306216fcf1504ece8e850c29a939b2245bea70af
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-19 13:25:20 -0500

    Add docstrings to functions

--- app/app.py
+++ app/app.py
@@ -14,8 +14,10 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+
     #Initialise Database
     db = SQLAlchemy(app)
+
 
     #temp data
     results = [{"strain_id": 1, "score": 80},
@@ -24,24 +26,57 @@ def create_app():
                 {"strain_id": 4, "score": 30},
                 {"strain_id": 5, "score": 30}]
 
+
     #temp "model"
     model = "pickled_model"
+
 
     #Home page urls
     @app.route("/")
     def home():
+        """Root page, you shoud not land here.
+        
+        Returns:
+            string -- Provides link to project home.
+        """
         return render_template('home.html')
 
 
     @app.route("/request", methods=['GET', 'POST'])
-    def request():
-        # user_info = request.args  # Is this correct?
-        user_info = "test_data"  # Temporary
-        results = get_preds(user_info)
+    def search(user_input=None):
+        """Takes in user input and predicts top five recommended strains
+        
+        Keyword Arguments:
+            user_input {str} -- [effects, ailments, and flavors to pass to model ]
+             (default: {None})
+        
+        Returns:
+            [ARRAY] -- Returns a List of recommended strains, and a score of recommendation strength
+            [EXAMPLE:] -- [{"strain_id": 1, "score": 80},
+                          {"strain_id": 2, "score": 50},
+                          {"strain_id": 3, "score": 40},
+                          {"strain_id": 4, "score": 30},
+                          {"strain_id": 5, "score": 30}]
+        """
+        user_input = user_input or request.values["user_input"]
+        results = get_preds(user_input)
         return jsonify(results)
+    
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return 'This page does not exist', 404
 
     def get_preds(user_info):
+        """Retrives prediction
+        
+        Arguments:
+            user_info {str} -- Returns a List of recommended strains, and a score of recommendation strength
+        
+        Returns:
+            list -- Predictions
+        """
         # return model.predict(user_info)
         return results
     
+
     return app
