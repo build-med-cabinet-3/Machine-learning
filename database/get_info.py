5b9499e8422637e1cf4292323cce09dd8327d704
PhatDeluxe <ethan@skamarock.net>
2019-11-20 11:33:27 -0700

    cleaned the code, and updated the db with Colin's updated dataset

--- database/get_info.py
+++ database/get_info.py
@@ -8,25 +8,31 @@ def strain_info(id_list, distance_list):
     and returns all the information about the strains
     in a json format"""
 
-
-
     #connectiong to the database
     sl_conn = sqlite3.connect('med_cabinet3.sqlite3')
     sl_curs = sl_conn.cursor()
 
+
     #initalizing the list to be returned
     return_list = []
+
 
     #the columns that will be retrieved from the database
     needed_columns = ['strain', 'effect', 'medical_effect_plain',
                       'flavor', 'Type', 'THC_Percent',
                       'CBD', 'Description1']
 
+
+    #Creating a list then adding key-value-pairs to said list
+    # ends by appending the key-value-pair list to another list as a key-value-pair 
     for i in range(0, 5):
         strain_list = []
         for item in needed_columns:
             request = f'SELECT {item} FROM strain_info WHERE id = {id_list[i]};'
             value = str(sl_curs.execute(request).fetchall())
+            #For some reason the SQL query returns something 
+            # formatted like (['<strain-name>,]) so this is 
+            # to remove all the useless characters
             value = value.replace(')', '')
             value = value.replace('[', '')
             value = value.replace(']', '')
