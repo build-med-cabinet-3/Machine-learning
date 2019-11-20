9304986bb50c5c7fdbf404fb39157797ddbdbfea
PhatDeluxe <ethan@skamarock.net>
2019-11-20 11:22:01 -0700

    Updated strain_info function in get_info to accept 2 lists of numbers and output a JSON formated object. Updated the database with new data. Commented and cleaned all files, still have more work to do

--- database/get_info.py
+++ database/get_info.py
@@ -3,28 +3,40 @@
 import sqlite3
 
 
-def strain_info(strain_id):
-    """takes in the strain id and returns all the information about the strain"""
+def strain_info(id_list, distance_list):
+    """takes in the list of two arrays returned from the model
+    and returns all the information about the strains
+    in a json format"""
+
+
+
+    #connectiong to the database
     sl_conn = sqlite3.connect('med_cabinet3.sqlite3')
     sl_curs = sl_conn.cursor()
+
+    #initalizing the list to be returned
     return_list = []
 
+    #the columns that will be retrieved from the database
     needed_columns = ['strain', 'effect', 'medical_effect_plain',
                       'flavor', 'Type', 'THC_Percent',
                       'CBD', 'Description1']
 
-
-    for item in needed_columns:
-        request = f'SELECT {item} FROM strain_info WHERE id = {strain_id};'
-        value = str(sl_curs.execute(request).fetchall())
-        value = value.replace(')', '')
-        value = value.replace('[', '')
-        value = value.replace(']', '')
-        value = value.replace('(', '')
-        value = value.replace(',', '')
-        value = value.replace("'", '')
-        feature = {item: value}
-        return_list.append(feature)
-
+    for i in range(0, 5):
+        strain_list = []
+        for item in needed_columns:
+            request = f'SELECT {item} FROM strain_info WHERE id = {id_list[i]};'
+            value = str(sl_curs.execute(request).fetchall())
+            value = value.replace(')', '')
+            value = value.replace('[', '')
+            value = value.replace(']', '')
+            value = value.replace('(', '')
+            value = value.replace(',', '')
+            value = value.replace("'", '')
+            feature = {item: value}
+            strain_list.append(feature)
+        score = {'Score': distance_list[i]}
+        strain_list.append(score)
+        return_list.append({i+1: strain_list})
     sl_curs.close()
     return return_list
--- database/set_to_base.py
+++ database/set_to_base.py
@@ -8,28 +8,35 @@ import pandas as pd
 - thc content
 - cbd content
 - 1 description
-- medical effects
+- medical effects plain
 - flavor
 - effects
 """
 
-
+#list of dataset columns we want in the database
 needed_columns = ['id', 'strain', 'effect', 'medical_effect_plain', 'flavor', 'Type', 'THC_Percent',
                   'CBD', 'Description1']
 
+
+#importing the dataset
 df1 = pd.read_csv('C:/Users/PhatDeluxe/buildweek_unit3/Machine-learning/database/no_nan_data.csv')
 
 df = pd.DataFrame()
 
+
+#creating a dataframe with just the above columns
 for item in needed_columns:
     df[item] = df1[item]
 
+
+#initalizing the database
 sl_conn = sqlite3.connect('med_cabinet3.sqlite3')
 sl_curs = sl_conn.cursor()
 
-model_id = 1
-
+#putting the trimmed dataframe into the database
 df.to_sql('strain_info', con=sl_conn)
 
 sl_curs.close()
 sl_conn.commit()
+
+print('Conversion was successful!')
