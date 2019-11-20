51a6d36fc10c18ecc86b3071d87431603d2c2b97
HakujouRyu <47827212+HakujouRyu@users.noreply.github.com>
2019-11-20 10:03:09 -0500

    Update and move model, Changing nlp path for local

--- model/model.py
+++ model/model.py
@@ -105,11 +105,11 @@ params = {
 # Load spacy model
 
 # Use if deploying to heroku.  manually add folder to base repo
-path_to_model = os.path.join(os.getcwd(), "en_core_web_md-2.2.0/")
+# path_to_model = os.path.join(os.getcwd(), "en_core_web_md-2.2.0/")
 
 # Use if local/pushing to github.  Requires installation of model via
 #    python -m spacy download en_core_web_md
-# path_to_model = "en_core_web_md"
+path_to_model = "en_core_web_md"
 
 nlp = spacy.load(path_to_model)
 
